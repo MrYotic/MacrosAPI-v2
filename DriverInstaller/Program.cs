39c1e80020078e5045813daca9add106dc83111c
Nekiplay <Nekiplay@DESKTOP-RKN5HA1>
2021-12-02 03:22:16 +0500

    DriverInstaller

---
+++ DriverInstaller/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriverInstaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile("https://github.com/Nekiplay/MacrosAPI-v2/raw/master/Driver/Driver.zip", "Driver.zip");
+
+                if (File.Exists("Driver.zip"))
+                {
+                    Directory.CreateDirectory("Driver");
+                    try { ZipFile.ExtractToDirectory("Driver.zip", "Files"); } catch { }
+                    try { Process.Start("Files\\Driver\\InstallDriver.bat"); } catch { }
+                }
+            }
+        }
+    }
+}
